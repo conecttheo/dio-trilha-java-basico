1e95bf8e43e26467fb5d5d013784ddfa328f884c
conecttheo <theodamata@gmail.com>
2024-05-16 16:32:59 -0300

    aula-tipos e variaveis

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String meuNome = "theodoro";
+
+        System.out.println(meuNome);
+
+        //variavel
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        //constante (SEMPRE COM final E UPPERCASE)
+        final double VALOR_DE_PI = 3.14;
+
+        //VALOR_DE_PI = 10.15
+
+        System.out.println(VALOR_DE_PI);
+    }
+}
